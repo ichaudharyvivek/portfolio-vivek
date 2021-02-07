70b9cdcfd56f12d52111fe446af17578032ad308
Vivek Kumar Chaudhary <vkc369258@gmail.com>
2021-02-07 19:43:00 +0530

    Preloader animationa and page transition complete.

--- public/javascript/session.js
+++ public/javascript/session.js
@@ -1,12 +1,8 @@
-// window.onload = () => {
-//   if (sessionStorage.getItem("splash") !== "true") {
-//     sessionStorage.setItem("splash", "true");
-//     document.querySelector("#transition").style.display = "none";
-//   } else {
-//     document.querySelector(".splash").style.display = "none";
-//   }
-// };
-
-// window.onbeforeunload = () => {
-//   sessionStorage.removeItem("splash");
-// };
+document.addEventListener("DOMContentLoaded", () => {
+  if (!sessionStorage.getItem("doNotShow")) {
+    sessionStorage.setItem("doNotShow", true);
+    document.querySelector(".splash").style.display = "flex";
+  } else {
+    document.querySelector(".splash").style.display = "none";
+  }
+});
