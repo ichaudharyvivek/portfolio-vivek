bede432da87a075d0671141b0985693099d8f55c
Vivek Kumar Chaudhary <vkc369258@gmail.com>
2021-02-07 15:32:42 +0530

    Finish page transition. ToDo: BugFix splash screen to load it only once.

---
+++ public/javascript/session.js
@@ -0,0 +1,12 @@
+// window.onload = () => {
+//   if (sessionStorage.getItem("splash") !== "true") {
+//     sessionStorage.setItem("splash", "true");
+//     document.querySelector("#transition").style.display = "none";
+//   } else {
+//     document.querySelector(".splash").style.display = "none";
+//   }
+// };
+
+// window.onbeforeunload = () => {
+//   sessionStorage.removeItem("splash");
+// };
